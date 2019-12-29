fa5594694e35ffabbd1dbcca1bc3b6840e827b7b
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-12-29 01:41:16 -0500

    Update build-pkgs-and-deploy.sh

--- build-pkgs-and-deploy.sh
+++ build-pkgs-and-deploy.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/c4pt000/Glomp
+#git clone https://github.com/c4pt000/Glomp
+#cd Glomp/
 
-cd Glomp/
 yum install gtk-sharp* gtk-sharp*devel mesa* gnome-sharp gnome-vfs2*devel gnome-vfs2*
 mono bin/Debug/glomp.exe
